bc9c64ac9b5caad8868a08846c7faff0bd6c3a64
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-04-15 20:12:43 +0530

    Time: 3 ms (57.72%), Space: 8.5 MB (42.91%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+vector<int> inorderTraversal(TreeNode* root) {
+ //using morris traversal
+ vector<int>res;
+ TreeNode* cur=root;
+ while(cur!=NULL){
+     if(cur->left==NULL){
+         res.push_back(cur->val);
+         cur=cur->right;
+     }else{
+         TreeNode* prev=cur->left;
+         //move to the right most node in the cur's left subtree
+         //while checking if there is no thread already present
+         while(prev->right  && prev->right!=cur) {
+             prev=prev->right;
+         }
+         //There are two possible outcome:
+         if(prev->right==NULL){
+             prev->right=cur;
+             cur=cur->left;
+         }else{
+             //break the existing thread and push the current val
+             prev->right=NULL;
+             res.push_back(cur->val);
+             cur=cur->right;
+         }
+         
+     }
+ }
+ return res;
+ 
+
+    }
+};
