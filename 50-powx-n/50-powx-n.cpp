ac5dbfe745a6175db4c50142e48c26642864cf77
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-04-19 19:55:27 +0530

    Time: 0 ms (100.00%), Space: 6 MB (45.64%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    double myPow(double x, int b) {
-        double res=1;
-        while(b){
-            if(b%2) res=b>0?res*x:res/x;
-                x=x*x;
-                b/=2;
-                  
-            
-                
-            }
+    double myPow(double x, int n) {;
+     double res = 1;
+        while (n) {
+            if (n % 2) res = n > 0 ? res * x : res / x;
+            x = x * x;
+            n /= 2;
+        }
         return res;
+    
         }
         
 };
+
+
+
+//         while(b){
+//             if(b%2) res=b>0?res*x:res/x;
+//                 x=x*x;
+//                 b/=2;
+                  
+            
+                
+//             }
+//         return res;
