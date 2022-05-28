e3d30ddee1177ba8900123626376811f0afb96b1
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-05-28 20:21:02 +0530

    Time: 74 ms (16.81%), Space: 29.9 MB (9.30%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,23 +1,49 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        unordered_map<char,int> mp,window;
-        int n1 = s1.size(),n2 = s2.size();
-        if(n1>n2) return false;
-        for(int i=0; i<n1 ; i++){
-            mp[s1[i]]++;
-            window[s2[i]]++;
-        }
-        if(window == mp)return true;
-        int k=0;
-        for(int i=n1 ; i<n2 ; i++){
-           if(window[s2[k]] == 1) window.erase(s2[k]);
-            else window[s2[k]]--;
-            window[s2[i]]++;
-            k++;
-            if(mp == window) return true;
-        }
-        return false;
+
     
+    
+    bool are_equal(vector<int> sone,vector<int> stwo){
+        for(int i=0;i<26;i++)
+        {
+            
+           if(sone[i]!=stwo[i]){
+            return false;
+        }
+        }
+       return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+    if(s2.size()<s1.size()){
+        return false;
+    }
+        
+    
+        
+        vector<int> freq_s1(26,0);
+        for(char c: s1){
+            freq_s1[c-'a']++;
+        }
+        
+        vector<int> freq_s2(26,0);
+        
+        int i=0;
+        int j=0;
+        while(j<s2.size()){
+            freq_s2[s2[j]-'a']++;
+        
+        if(j-i+1==s1.size()){
+            if(are_equal(freq_s1,freq_s2)) return true;
+        }
+        if(j-i+1<s1.size()){
+            j++;
+        }
+        else{
+            freq_s2[s2[i]-'a']--;
+            i++;
+            j++;
+        }
+        }
+      return false;
     }
 };
