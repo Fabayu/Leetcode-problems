67b7abfa3070e13e904c7ea6ee541b9e61f3c545
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-05-29 10:21:49 +0530

    Time: 0 ms (100.00%), Space: 7 MB (31.49%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,46 +1,82 @@
 class Solution {
 public:/*
+[1,2,3]
+[4,5,6]
+[7,8,9]
+
+
+[1,1,1]
+[0,0,1]
+[0,0,1]
 */
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> res;
-        if(matrix.size()==0) return res;
-        int minrow=0;
-         
-        
-        int maxrow=matrix.size()-1;
-        int maxcol=matrix[0].size()-1;
-        int mincol=0;
-        while(minrow<=maxrow&&mincol<=maxcol){
-           
-            for(int i=mincol;i<=maxcol;i++){
-                res.push_back(matrix[minrow][i]);
-                
+        vector<int>res;
+     int i=0;
+        int j=0;
+        int v1=0;
+        int v2=0;
+        int r=matrix.size();
+        int c=matrix[0].size();
+        int vis[r][c];
+        memset(vis,0,sizeof(vis));
+        int count=0;
+        while(i>=0&&j>=0&&i<r&&j<c){
+         //   cout<<count<<" ";
+          //  count++;
+            v2=1;
+            while(i>=0&&j>=0&&i<r&&j<c){
+                res.push_back(matrix[i][j]);
+                cout<<matrix[i][j]<<" ";
+                vis[i][j]=1;
+               i= i+v1;
+               j= j+v2;
+                 if(i>=0&&j>=0&&i<r&&j<c&&vis[i][j]==1)break;
             }
-            minrow++;
-            
-            
-            for(int i=minrow;i<=maxrow;i++){
-                res.push_back(matrix[i][maxcol]);
+             if(res.size()==r*c)break;
+            j=j-1;
+            i=i+1;
+            v1=1;
+            v2=0;
+            while(i>=0&&j>=0&&i<r&&j<c){
+                 res.push_back(matrix[i][j]);
+                 cout<<matrix[i][j]<<" ";
+                vis[i][j]=1;
+                i= i+v1;
+               j= j+v2;  
+                if(i>=0&&j>=0&&i<r&&j<c&&vis[i][j]==1)break;
             }
-            maxcol--;
-            
-            if(minrow<=maxrow){
-                for(int i=maxcol;i>=mincol;i--){
-                    res.push_back(matrix[maxrow][i]);
-                }
+             if(res.size()==r*c)break;
+            i=i-1;
+            j=j-1;
+            v1=0;
+            v2=-1;
+             while(i>=0&&j>=0&&i<r&&j<c){
+                  res.push_back(matrix[i][j]);
+                  cout<<matrix[i][j]<<" ";
+                 vis[i][j]=1;
+                i= i+v1;
+               j= j+v2;  
+                 if(i>=0&&j>=0&&i<r&&j<c&&vis[i][j]==1)break;
             }
-                maxrow--;
-                
-                if(mincol<=maxcol){
-                    for(int i=maxrow;i>=minrow;i--){
-                        res.push_back(matrix[i][mincol]);
-                    }
-                }
-                            
-            mincol++;
+             if(res.size()==r*c)break;
+            j=j+1;
+            i=i-1;
+            v1=-1;
+            v2=0;
+             while(i>=0&&j>=0&&i<r&&j<c){
+                  res.push_back(matrix[i][j]);
+                  cout<<matrix[i][j]<<" ";
+                 vis[i][j]=1;
+                i= i+v1;
+               j= j+v2;  
+                 if(i>=0&&j>=0&&i<r&&j<c&&vis[i][j]==1)break;
             }
+            i=i+1;
+            j=j+1;
+            v1=0;
             
-        
+            if(res.size()==r*c)break;
+        }
         return res;
     }
 };
