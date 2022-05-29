c44958b5280750b6b435fe5b330447edd5bdc52f
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-05-29 22:29:12 +0530

    Added solution - LeetHub

--- Common elements - GFG/common-elements.cpp
+++ Common elements - GFG/common-elements.cpp
@@ -18,60 +18,60 @@ class Solution
     */
        
            
-       vector <int> commonElements (int ar1[], int ar2[], int ar3[], int n1, int n2, int n3)
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
         {
          vector<int> res;
          
-        //  int i=0;
-        //  int j=0;
-        //  int k=0;
-        //  int prev1,prev2,prev3=INT_MIN;
-        //  while(i<n1&&j<n2&&k<n3){
-        //      while(i<n1 && prev1==A[i]){
-        //          i++;
-        //      }
-        //      while(j<n2 && prev2==B[j]){
-        //          j++;
-        //      }
-        //      while(k<n3 && prev3==C[k]){
-        //          k++;
-        //      }
+         int i=0;
+         int j=0;
+         int k=0;
+          int prev1,prev2,prev3;
+         prev1=prev2=prev3=INT_MIN;
+         while(i<n1&&j<n2&&k<n3){
+             while(i<n1 && prev1==A[i]){
+                 i++;
+             }
+             while(j<n2 && prev2==B[j]){
+                 j++;
+             }
+             while(k<n3 && prev3==C[k]){
+                 k++;
+             }
              
              
-        //      if((A[i]==B[j])&&(B[j]==C[k]))
-        //      {
-        //          res.push_back(A[i]);
-        //          prev1=A[i];
-        //          prev2=B[j];
-        //          prev3=C[k];
-        //          i++;
-        //          j++;
-        //          k++;
-        //      }
-        //      else if(A[i]<B[j]){
+             if((A[i]==B[j])&&(B[j]==C[k]))
+             {
+                 res.push_back(A[i]);
+                 prev1=A[i];
+                 prev2=B[j];
+                 prev3=C[k];
+                 i++;
+                 j++;
+                 k++;
+             }
+             else if(A[i]<B[j]){
                  
-        //          prev1=A[i];
-        //          i++;
-        //      }
-        //      else if(B[j]<C[k]){
+                 prev1=A[i];
+                 i++;
+             }
+             else if(B[j]<C[k]){
                  
-        //          prev2=B[j];
-        //          j++;
-        //      }
-        //      else {
+                 prev2=B[j];
+                 j++;
+             }
+             else {
 
-        //          prev3=C[k];
+                 prev3=C[k];
                  
-        //                           k++;
-        //      }
+                                  k++;
+             }
              
              
              
-        //  }
-        // return res;
+         }
+        return res;
             
-        //   //code here.
-        // }
+          //code here.
         
         
         
@@ -79,82 +79,7 @@ class Solution
         
         
         
-//         findCommon(int ar1[], int ar2[], int ar3[], int n1,
-//                 int n2, int n3)
-// {
- 
-    // Initialize starting indexes
-    // for ar1[], ar2[] and
-    // ar3[]
-    int i = 0, j = 0, k = 0;
- 
-    // Declare three variables prev1,
-    // prev2, prev3 to track
-    // previous element
-    int prev1, prev2, prev3;
- 
-    // Initialize prev1, prev2,
-    // prev3 with INT_MIN
-    prev1 = prev2 = prev3 = INT_MIN;
- 
-    // Iterate through three arrays
-    // while all arrays have
-    // elements
-    while (i < n1 && j < n2 && k < n3) {
- 
-        // If ar1[i] = prev1 and i < n1,
-        // keep incrementing i
-        while (ar1[i] == prev1 && i < n1)
-            i++;
- 
-        // If ar2[j] = prev2 and j < n2,
-        // keep incrementing j
-        while (ar2[j] == prev2 && j < n2)
-            j++;
- 
-        // If ar3[k] = prev3 and k < n3,
-        // keep incrementing k
-        while (ar3[k] == prev3 && k < n3)
-            k++;
- 
-        // If x = y and y = z, print
-        // any of them, update
-        // prev1 prev2, prev3 and move
-        // ahead in each array
-        if (ar1[i] == ar2[j] && ar2[j] == ar3[k]) {
-            res.push_back(ar1[i]);
-            prev1 = ar1[i];
-            prev2 = ar2[j];
-            prev3 = ar3[k];
-            i++;
-            j++;
-            k++;
-        }
- 
-        // If x < y, update prev1
-        // and increment i
-        else if (ar1[i] < ar2[j]) {
-            prev1 = ar1[i];
-            i++;
-        }
- 
-        // If y < z, update prev2
-        // and increment j
-        else if (ar2[j] < ar3[k]) {
-            prev2 = ar2[j];
-            j++;
-        }
- 
-        // We reach here when x > y
-        // and z < y, i.e., z is
-        // smallest update prev3
-        // and imcrement k
-        else {
-            prev3 = ar3[k];
-            k++;
-        }
-    }
-    return res;
+
 }
 
 };
