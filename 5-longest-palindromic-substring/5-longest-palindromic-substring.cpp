0f2b512260dbfc23385c569c276c1342a12e9758
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-06-16 23:45:04 +0530

    Time: 4 ms (99.16%), Space: 7.2 MB (81.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -12,9 +12,9 @@ public:
     for (int i = 0; i < s.size();) {
       if (s.size() - i <= max_len / 2) break;
       int j = i, k = i;
-      while (k < s.size()-1 && s[k+1] == s[k]) ++k; // Skip duplicate characters.
+      while (k < s.size()-1 && s[k+1] == s[k]) ++k;
       i = k+1;
-      while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
+      while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } 
       int new_len = k - j + 1;
       if (new_len > max_len) { min_start = j; max_len = new_len; }
     }
