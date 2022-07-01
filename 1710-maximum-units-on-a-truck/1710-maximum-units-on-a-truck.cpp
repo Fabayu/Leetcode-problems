38f4bc1ca17da19fa9ac064c3b1b11310d8c295e
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-07-01 05:52:21 +0530

    Time: 25 ms (99.83%), Space: 15.9 MB (77.60%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+    
+	int freq[1001]{0}, maxUnits = 0;   // freq[i] = number of boxes that can hold i units
+	for(auto& box : boxTypes) freq[box[1]] += box[0];
+	// greedily choose starting from max units till either truckSize runs out or you choose all boxes
+	for(int units = 1000; truckSize > 0 && ~units; --units) { 
+		maxUnits += min(truckSize, freq[units]) * units;
+		truckSize -= freq[units];
+	}
+	return maxUnits;
+
+   
+    }
+};
