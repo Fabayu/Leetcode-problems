d311072971aae2f4dc11618483b914ac35d0b29a
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-07-15 22:41:42 +0530

    Added solution - LeetHub

---
+++ Odd Game - GFG/odd-game.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    long long oddGame(long long N) {
+ if(N==1){
+     return 1;
+ }
+ return oddGame(N/2)*2;
+ }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.oddGame(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
