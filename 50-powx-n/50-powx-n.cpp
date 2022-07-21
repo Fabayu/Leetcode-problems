7b6d9ebdf02274a0a3f0fb768e47e6bd6b145744
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-07-21 12:40:49 +0530

    Time: 4 ms (32.24%), Space: 6 MB (43.20%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,17 +1,51 @@
+
 class Solution {
 public:
-    double myPow(double x, int n) {;
-    double res=1;
-    while(n){
-        if(n%2) res=n>0?res*x:res/x;
-        x=x*x;
-        n/=2;
-    }
-     return res;
+    double find(double x,int n){
+      if(n==0)return 1.0;
+       if(x==0) return 0.0;
+       else{
+           double res=1.0; 
+        res=find(x,n/2);
+        res=res*res;
+        if(n%2==0){
+         return res;   
         }
+           else{
+               return x*res;
+           }
+       }
+    }
+    double myPow(double x, int n) {
+        
+        double res=1.0;
+        res=find(x,abs(n));
+        if(n>=0){
+           return res; 
+        }
+        else{
+            return (1/res);
+        }
+    }
         
 };
+    
+    
+    
+    
+    
+    
+    
 
+
+//  double res=1;
+//     while(n){
+//         if(n%2) res=n>0?res*x:res/x;
+//         x=x*x;
+//         n/=2;
+//     }
+//      return res;
+//         }
 
 
 //         while(b){
