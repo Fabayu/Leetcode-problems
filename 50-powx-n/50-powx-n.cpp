1909ad72a2626bb14ad64b0ef1df1fa1229f3fea
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-07-21 12:49:32 +0530

    Time: 2 ms (53.33%), Space: 5.9 MB (79.99%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -2,27 +2,40 @@
 class Solution {
 public:
     double find(double x,int n){
+        //base cases
       if(n==0)return 1.0;
        if(x==0) return 0.0;
+        
+        
        else{
            double res=1.0; 
-        res=find(x,n/2);
-        res=res*res;
-        if(n%2==0){
+        res=find(x,n/2);  // half krle half mekr lunga 
+        res=res*res;      // idhr aa half me de rha
+        if(n%2==0){        // check for even  [2^6= (2*2*2 ....half)]
          return res;   
         }
            else{
-               return x*res;
+               return x*res;     // check for odd{2^5=(2*...half .....itself)}
            }
        }
     }
     double myPow(double x, int n) {
         
+        
+        
+        
+        //double since res can be in decimal
         double res=1.0;
+        
+        //recursive call on abs n if pos good nega go to quotient
         res=find(x,abs(n));
+        
+        
+        //if n>=0 res will be returned 
         if(n>=0){
            return res; 
         }
+        //otherwise  school like div
         else{
             return (1/res);
         }
