f792c0f23be03c8755f13b2fa6d45077858ce23b
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-07-21 11:19:51 +0530

    Time: 3 ms (98.55%), Space: 10.7 MB (93.52%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+         if (k >= prices.size() / 2) { // if k >= n/2, then you can make maximum number of transactions
+        int profit = 0;
+        for (int i = 1; i < prices.size(); i++)
+            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        return profit;
+    }
+   vector<int> buy(k+1,INT_MAX);
+   vector<int> sell(k + 1,0);
+
+    for (int price : prices) {
+        for (int i = 1; i <= k; i++) {
+            buy[i] = min(buy[i], price-sell[i - 1]);
+            sell[i] = max(sell[i], price-buy[i]);
+        }
+    }
+    return sell[k];
+  
+    }
+};
