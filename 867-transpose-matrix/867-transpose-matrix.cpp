60656e325c0482afc08994f43dfd967d7edaa466
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-07-24 15:26:46 +0530

    Time: 23 ms (25.79%), Space: 10.6 MB (40.70%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -5,6 +5,7 @@ public:
        
         int col=matrix[0].size();
          vector<vector<int>> res(col,vector<int> (row,0) );
+ 
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 res[j][i]=matrix[i][j];
