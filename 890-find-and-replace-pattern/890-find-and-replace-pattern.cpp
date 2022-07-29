116b22529e400129791e63d6df09f594bac59200
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-07-29 07:47:12 +0530

    Time: 7 ms (55.39%), Space: 8.4 MB (53.91%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    vector<string> findAndReplacePattern(vector<string>& words, string p) {
-     
-        vector<string> res;
-        p = U_calci(p);
-        for (string w : words) if (U_calci(w) == p) res.push_back(w);
-        return res;   
-    }
-     string U_calci(string w) {
-        unordered_map<char, int> m;
-        for (char c : w) if (!m.count(c)) m[c] = m.size();
-        for (int i = 0; i < w.length(); ++i) w[i] = 'a' + m[w[i]];
-        return w;
-     }
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+
+/*
+    convert each string to a base / standard pattern and then compare them. Here, we are converting all occurences of first found character to a, second all occurences of second found character to b, then c and so on...
+
+---->> Consider the words "mmnpoq" and "ppqqqz". We can convert these into - "aabcde" and "aabbbc". Since they are not equal, we can say both these words don't match.
+Similarly, the words "mnpqrr" and "xyzabb" can be converted into "abcdee" and "abcdee" respectively. Since they are equal, we say that these two words match.
+    */
+        
+        
+vector<string> ans;
+	string p = normalise(pattern);
+	for(auto& w : words) 
+		if(p == normalise(w)) ans.push_back(w);
+	return ans;
+}
+string normalise(string w) {
+	unordered_map<char, char> mp;
+	char c = 'a';
+	for(auto ch : w) 
+		if(!mp.count(ch)) mp[ch] = c++;
+	for(auto& ch : w) ch = mp[ch];
+	return w;
+}
 };
