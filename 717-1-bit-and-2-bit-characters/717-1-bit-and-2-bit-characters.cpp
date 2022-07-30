abac2ad1389bdfe35fabfb60051a355a4204975f
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-07-31 00:43:22 +0530

    Time: 8 ms (42.82%), Space: 9.7 MB (64.23%) - LeetHub

--- 717-1-bit-and-2-bit-characters/717-1-bit-and-2-bit-characters.cpp
+++ 717-1-bit-and-2-bit-characters/717-1-bit-and-2-bit-characters.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isOneBitCharacter(vector<int>& bits) {
-      int count_1 = 0;
-        for (int i = (int) bits.size() - 2; i >= 0 && bits[i] == 1; i--) count_1++;
-        return (count_1 % 2 == 0);  
+      int count_ing = 0;
+        for (int i = (int) bits.size() - 2; i >= 0 && bits[i] == 1; i--) count_ing++;
+        return (count_ing % 2 == 0);  
     }
 };
