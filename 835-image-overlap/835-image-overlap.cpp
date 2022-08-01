6fe05f14ffa9e7b91fca0b75928bdf3b5ed74af1
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-08-01 21:16:04 +0530

    Time: 104 ms (79.33%), Space: 12 MB (49.04%) - LeetHub

---
+++ 835-image-overlap/835-image-overlap.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+         int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<int> ayush, jaiswal;
+        int N = A.size();
+             
+             
+        unordered_map<int, int> count;
+             
+             
+        for (int i = 0; i < N * N; ++i)
+            if (A[i / N][i % N] == 1)
+                ayush.push_back(i / N * 100 + i % N);
+        for (int i = 0; i < N * N; ++i)
+            if (B[i / N][i % N] == 1)
+                jaiswal.push_back(i / N * 100 + i % N);
+        for (int i : ayush) for (int j : jaiswal) count[i - j]++;
+        int res = 0;
+        for (auto it : count) res = max(res, it.second);
+        return res;
+    
+    }
+};
