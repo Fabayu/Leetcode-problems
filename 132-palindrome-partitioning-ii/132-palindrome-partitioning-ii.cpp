a4460416f003d95c2cbd5ba08078ef16f1c508fe
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-08-01 22:58:32 +0530

    Time: 20 ms (96.15%), Space: 6.5 MB (79.91%) - LeetHub

---
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,19 @@
+
+
+class Solution {
+public:
+    int minCut(string s) {
+        int n = s.size();
+        vector<int> dp(n+1, INT_MAX);
+        dp[0] = -1;       //act as a sentinel
+        for(int i = 0; i < n; i++){
+	//palindrome of length 1,3,5...
+            for(int len = 0; i-len >= 0 && i+len < n && s[i-len] == s[i+len]; len++)
+                dp[i+len+1] = min(dp[i+len+1], dp[i-len]+1);
+	//palindrome of lenght 2,4,6...
+            for(int len = 0; i-len >= 0 && i+len+1 < n && s[i-len] == s[i+len+1]; len++)
+                dp[i+len+2] = min(dp[i+len+2], dp[i-len]+1);
+        }
+        return dp[n];
+    }
+};
