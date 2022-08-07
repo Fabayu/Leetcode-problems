6bf1aa311ac0e0002759bb955a5466845de24f04
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-08-07 12:25:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (95.33%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -3,39 +3,18 @@
 class Solution {
 public:
     int countVowelPermutation(int n) {
-        long a = 1, e = 1, i = 1, o = 1, u = 1, mod = pow(10, 9)+7;
-        long a2, e2, i2, o2, u2; 
-       
-        
-        
-        
-       
-        for (int j = 2; j <= n; j++) {
-            a2 = (e + i + u) % mod;
-            e2 = (a + i) % mod;
-            i2 = (e + o) % mod;
-            o2 = i;
-            u2 = (o + i) % mod;
+       long a=1,e=1,i=1,o=1,u=1;
+        long   a2,e2,i2,o2,u2;
+        long mod=pow(10,9)+7;
+        for(int k =2;k<=n;k++){
+            a2=(e+i+u)%mod;
+            e2=(a+i)%mod;
+            i2=(e+o)%mod;
+                o2=(i)%mod;
+                u2=(o+i)%mod;
             
-            a = a2, e = e2, i = i2, o = o2, u = u2;
-            
-            
-            /* say for n==2
-       a2=3;
-       e2=2;
-       i2=2;
-       02=1;
-       u2=2;
-       now update all values and go for next iteration
-       
-       
-       
-         */
-            
-            
-            
+            a=a2,e=e2,i=i2,o=o2,u=u2;
         }
-        
-        return (a + e + i + o + u) % mod;
+        return (a+e+i+o+u)%mod;
     }
 };
