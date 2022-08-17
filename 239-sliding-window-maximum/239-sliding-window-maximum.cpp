75af5c3d32b8913b74dc30b85e0fbc88ef7d3bb6
Ayush Jaisi <87146546+Fabayu@users.noreply.github.com>
2022-08-17 19:03:02 +0530

    Time: 385 ms (76.97%), Space: 134.7 MB (62.80%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -7,16 +7,20 @@ public:
         
         deque<int> dq;
 
-     
-            for (int i = 0; i < nums.size(); i++) {
-    if (!dq.empty() && dq.front() == i - k) dq.pop_front();
-
-    while (!dq.empty() && nums[dq.back()] < nums[i])
-      dq.pop_back();
-
-    dq.push_back(i);
-    if (i >= k - 1) ans.push_back(nums[dq.front()]);
-  
+        for(int i=0;i<nums.size();i++)
+        {
+            
+            //first we will remove out of bound elemets (pop-front)
+            if(!dq.empty()&&dq.front()==i-k)dq.pop_front();
+            //we will keep popping fro back till elemetnt at last is less than incoming 
+            while(!dq.empty()&&nums[i]>nums[dq.back()])dq.pop_back();
+            dq.push_back(i);
+            
+            if(i>=k-1)ans.push_back(nums[dq.front()]);
+            
+            
+            
+       
     }
         return ans;
     }
