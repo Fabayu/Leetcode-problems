22d96fe785697ef994b9654eb898004dfb10a04a
Ayush Jaiswal <87146546+Fabayu@users.noreply.github.com>
2022-09-06 01:21:33 +0530

    Time: 39 ms (51.18%), Space: 11.9 MB (65.11%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,47 @@
+
+
+// //Approach :
+// -->>Case of unbounded knapsack with like of subset problem
+// --->>steps::
+// 1.Initialise dp array 
+
+
+
+class Solution {
+public:
+
+
+    int change(int a, vector<int>& coins) {
+     int n=coins.size();
+     int dp[n+1][a+1];
+     for(int i=0;i<n+1;i++){
+     for(int j=0;j<a+1;j++){
+         if(i==0){
+         dp[i][j]=0;
+         
+         }
+         else if(j==0){
+         dp[i][j]=1;
+         }
+     }
+     
+     
+     
+     }
+     for(int i=1;i<n+1;i++){
+     for(int j=1;j<a+1;j++){
+     if(coins[i-1]<=j){
+     dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+     }
+     else{
+     dp[i][j]=dp[i-1][j];
+     }
+     }
+     }
+     return dp[n][a];
+     
+     
+     
+     
+    }
+};
