abc59a9665fbaccde55bdf316131fc691e7e1a42
Ayush Jaiswal <87146546+Fabayu@users.noreply.github.com>
2022-09-13 03:11:18 +0530

    Time: 439 ms (9.64%), Space: 103.5 MB (7.72%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-      int tortoise=nums[0];
-      int rabbit=nums[0];
-      do{
-          rabbit=nums[nums[rabbit]];
-          tortoise=nums[tortoise];
-          
-      }  while(rabbit!=tortoise);
-        rabbit=nums[0];
-        while(rabbit!=tortoise){
-            tortoise=nums[tortoise];
-            rabbit=nums[rabbit];
+    int findDuplicate(vector<int>& n) {
+        map<int,int> m;
+        int as;
+        for(auto x:n){
+            m[x]++;
         }
-        return rabbit;
+    for(auto it : m){
+            if(it.second>1){
+                as=it.first;
+            }
+        }
+        return as;
     }
 };
