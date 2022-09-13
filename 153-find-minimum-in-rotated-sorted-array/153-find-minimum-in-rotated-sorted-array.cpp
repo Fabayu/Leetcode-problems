c98a79df21543cb180f14135fd9947fb93e6a7e7
Ayush Jaiswal <87146546+Fabayu@users.noreply.github.com>
2022-09-14 00:48:05 +0530

    Time: 13 ms (7.99%), Space: 10.2 MB (71.26%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-        int lo=0;
-        int hi=nums.size()-1;
-        while(lo<hi){
-            int mid=lo+(hi-lo)/2;
-            if(nums[mid]>nums[hi]){
-                lo=mid+1;
-            }    
-                else{
-                    hi=mid;
-                
-            }
-        }
-        return nums[lo];
+    int findMin(vector<int>& arr){
+        	    
+	     int l= 0, h = arr.size()-1;
+       while(l<=h){
+           int mid = l + (h-l)/2;
+           
+           if(arr[mid]>arr[arr.size()-1]){
+               l = mid+1;
+           }
+           else{
+               
+           h = mid-1;
+           
+       }
+           
+       }
+       
+       return arr[l];
+	
     }
 };
+
+
+// noticed what?     Ayush  :)
+    
+    
+// :  in even        ..    ..   (low->mid    mid->+1,high)   equal distrib
+// :in odd           ...   ..    (low->mid    mid->+1,high)  not equal
+// :lo<hi      because if single no need to do anything
+// :if mid<hi   go search on left  ^^^^^^^^
+// :if mid >hi  idhr hi h idhr hi h on right side
+
+
+
+
+    
