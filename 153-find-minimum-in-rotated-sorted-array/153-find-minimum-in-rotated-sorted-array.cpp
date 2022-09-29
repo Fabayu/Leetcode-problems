82ac44e115f4bebed623e0a39f75925207af637f
Ayush <87146546+Fabayu@users.noreply.github.com>
2022-09-30 01:32:40 +0530

    Time: 3 ms (88.48%), Space: 10.3 MB (22.12%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
     int findMin(vector<int>& arr){
-        	    
-	     int l= 0, h = arr.size()-1;
-       while(l<=h){
-           int mid = l + (h-l)/2;
-           
-           if(arr[mid]>arr[arr.size()-1]){
-               l = mid+1;
-           }
-           else{
-               
-           h = mid-1;
-           
-       }
-           
-       }
-       
-       return arr[l];
-	
+    int lo=0;
+        int hi=arr.size()-1;
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if(arr[lo]<arr[hi]){
+                return arr[lo];
+            }
+            if(arr[mid]>arr[hi]){
+                lo=mid+1;
+            }
+            else{
+                hi=mid;
+            }
+        }
+	return arr[lo];
     }
-};
-
-
-// noticed what?     Ayush  :)
     
     
-// :  in even        ..    ..   (low->mid    mid->+1,high)   equal distrib
-// :in odd           ...   ..    (low->mid    mid->+1,high)  not equal
-// :lo<hi      because if single no need to do anything
-// :if mid<hi   go search on left  ^^^^^^^^
-// :if mid >hi  idhr hi h idhr hi h on right side
+};   
+    
+    
+    
+    
+    
+    
+//     Case 1. The leftmost value is less than the rightmost value in the list: This means that the list is not rotated.
+// e.g> [1 2 3 4 5 6 7 ]
+
+// Case 2. The value in the middle of the list is greater than the leftmost and rightmost values in the list.
+// e.g> [ 4 5 6 7 0 1 2 3 ]
+
+// Case 3. The value in the middle of the list is less than the leftmost and rightmost values in the list.
+// e.g> [ 5 6 7 0 1 2 3 4 ]
+
+// As you see in the examples above, if we have case 1, we just return the leftmost value in the list. If we have case 2, we just move to the right side of the list. If we have case 3 we need to move to the left side of the list.
+// };
+
 
 
 
