7b38fca9cf206642e4c3473b1703351743e68498
Ayush <87146546+Fabayu@users.noreply.github.com>
2022-09-30 00:58:36 +0530

    Time: 16 ms (6.18%), Space: 9.6 MB (51.16%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-       
+
         int i=0;
         int j=matrix[0].size()-1;
         while(j>=0&&i<matrix.size()){
@@ -14,26 +14,45 @@ public:
             else{
                 i++;
             }
-        }
+        }       
    return false;
     }
 };
 
-// sun phle tu intution ye h ki 
-// # make target the end value in any row if 
-// end vale se jada h target then next row me ja tjst is row++
-//     if target is smaleer than end value then j-- that is phle col me aa
+
+
+
+// int i=0;
+//         int j=matrix[0].size()-1;
+//         while(j>=0&&i<matrix.size()){
+//             if(target==matrix[i][j]){
+//                 return true;
+//             }
+//             else if(target<matrix[i][j]){
+//                 j--;
+//             }
+//             else{
+//                 i++;
+//             }
+//         }
+//    return false;
+//     // }
+
+// // sun phle tu intution ye h ki 
+// // # make target the end value in any row if 
+// // end vale se jada h target then next row me ja tjst is row++
+// //     if target is smaleer than end value then j-- that is phle col me aa
     
 
-//  int rows = matrix.size(),
-// 			cols = matrix[0].size(),
-//             row = 0, col = cols - 1;
+// //  int rows = matrix.size(),
+// // 			cols = matrix[0].size(),
+// //             row = 0, col = cols - 1;
 			
-//         while (row < rows && col > -1) {
-//             int cur = matrix[row][col];
-//             if (cur == target) return true;
-//             if (target > cur) row++;
-//             else col--;
-//         }
+// //         while (row < rows && col > -1) {
+// //             int cur = matrix[row][col];
+// //             if (cur == target) return true;
+// //             if (target > cur) row++;
+// //             else col--;
+// //         }
         
-//         return false;
+// //         return false;
