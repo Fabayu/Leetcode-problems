1e1e7571994516a0efc294617b0eb494c9ad151c
Ayush <87146546+Fabayu@users.noreply.github.com>
2022-10-08 00:43:07 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (98.73%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,19 +11,91 @@
  */
 
 
-//Recursive 
+// Single Stack
+
+//Go left left left and then right right right then print root
+//push into stack the print one then print
 class Solution {
 public:
-    void helper(vector<int> &ans,TreeNode* root){
-        if(root==NULL)return ;
-        helper(ans,root->left);
-        helper(ans,root->right);
-        ans.push_back(root->val);
-        
-    }
+   
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        helper(ans,root);
-        return ans;
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        stack<TreeNode*>st;
+        TreeNode *curr=root;
+        vector<int>v;
+        while(curr!=NULL||st.size()!=0){
+            if(curr!=NULL){
+                st.push(curr);
+                curr=curr->left;
+            }
+            else{
+                TreeNode *temp=st.top()->right;
+                if(temp==NULL){
+                    temp=st.top();
+                    st.pop();
+                    v.push_back(temp->val);
+                    while(st.size()!=0&&st.top()->right==temp){
+                        temp=st.top();
+                        v.push_back(temp->val);
+                        st.pop();
+                    }
+                }
+                else{
+                    curr=temp;
+                }
+            }
+        }
+        return v;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Recursive 
+// class Solution {
+// public:
+//     void helper(vector<int> &ans,TreeNode* root){
+//         if(root==NULL)return ;
+//         helper(ans,root->left);
+//         helper(ans,root->right);
+//         ans.push_back(root->val);
+        
+//     }
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         helper(ans,root);
+//         return ans;
+//     }
+// };
