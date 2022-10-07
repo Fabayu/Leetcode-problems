8342c34f80cb8b79a01b2b71eb051cfa04e0ff61
Ayush <87146546+Fabayu@users.noreply.github.com>
2022-10-07 22:28:19 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (75.05%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -12,36 +12,55 @@
 class Solution {
 public:
     
-vector<int> inorderTraversal(TreeNode* root) {
- //using morris traversal
- vector<int>res;
- TreeNode* cur=root;
- while(cur!=NULL){
-     if(cur->left==NULL){
-         res.push_back(cur->val);
-         cur=cur->right;
-     }else{
-         TreeNode* prev=cur->left;
-         //move to the right most node in the cur's left subtree
-         //while checking if there is no thread already present
-         while(prev->right  && prev->right!=cur) {
-             prev=prev->right;
-         }
-         //There are two possible outcome:
-         if(prev->right==NULL){
-             prev->right=cur;
-             cur=cur->left;
-         }else{
-             //break the existing thread and push the current val
-             prev->right=NULL;
-             res.push_back(cur->val);
-             cur=cur->right;
-         }
-         
-     }
- }
- return res;
- 
-
+    void helper(TreeNode* root,vector<int> &ans){
+        if(root==NULL){
+            return ;
+            
+        }
+        helper(root->left,ans);
+        ans.push_back(root->val);
+        helper(root->right,ans);
+        
     }
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    if(root==NULL) return ans ;
+    helper(root,ans);
+    return ans;
+}
 };
+
+
+
+
+
+
+//  //using morris traversal
+//  vector<int>res;
+//  TreeNode* cur=root;
+//  while(cur!=NULL){
+//      if(cur->left==NULL){
+//          res.push_back(cur->val);
+//          cur=cur->right;
+//      }else{
+//          TreeNode* prev=cur->left;
+//          //move to the right most node in the cur's left subtree
+//          //while checking if there is no thread already present
+//          while(prev->right  && prev->right!=cur) {
+//              prev=prev->right;
+//          }
+//          //There are two possible outcome:
+//          if(prev->right==NULL){
+//              prev->right=cur;
+//              cur=cur->left;
+//          }else{
+//              //break the existing thread and push the current val
+//              prev->right=NULL;
+//              res.push_back(cur->val);
+//              cur=cur->right;
+//          }
+         
+//      }
+//  }
+//  return res;
+ 
