d5e62f1f181d14497bc32d35bda525c040955146
Ayush <87146546+Fabayu@users.noreply.github.com>
2022-10-08 02:05:17 +0530

    Time: 4 ms (40.92%), Space: 8.4 MB (87.82%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,67 +11,41 @@
  */
 
 
-// Single Stack
-
-//Go left left left and then right right right then print root
-//push into stack the print one then print
+//2 stack
 class Solution {
 public:
    
     vector<int> postorderTraversal(TreeNode* root) {
-       
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        stack<TreeNode*>st;
-        TreeNode *curr=root;
-        vector<int>v;
-        while(curr!=NULL||st.size()!=0){
-            if(curr!=NULL){
-                st.push(curr);
-                curr=curr->left;
+             vector<int> ans;
+       stack<TreeNode*> s1,s2;
+        if(root==NULL)return ans;
+   
+        s1.push(root);
+        while(!s1.empty()){
+            
+            root=s1.top();
+            s2.push(root);
+            s1.pop();
+            if(root->left!=NULL){
+            s1.push(root->left);
             }
-            else{
-                TreeNode *temp=st.top()->right;
-                if(temp==NULL){
-                    temp=st.top();
-                    st.pop();
-                    v.push_back(temp->val);
-                    while(st.size()!=0&&st.top()->right==temp){
-                        temp=st.top();
-                        v.push_back(temp->val);
-                        st.pop();
-                    }
-                }
-                else{
-                    curr=temp;
-                }
-            }
+                if(root->right!=NULL){
+                s1.push(root->right);
+            } 
+                
+              
+                
+                
         }
-        return v;
+        while(!s2.empty()){
+            ans.push_back(s2.top()->val);
+            s2.pop();
+        }
+            return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
+      
 
 
 
@@ -99,3 +73,64 @@ public:
 //         return ans;
 //     }
 // };
+
+
+
+
+
+// Single Stack
+
+//Go left left left and then right right right then print root
+//push into stack the print one then print
+// class Solution {
+// public:
+   
+//     vector<int> postorderTraversal(TreeNode* root) {
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         stack<TreeNode*>st;
+//         TreeNode *curr=root;
+//         vector<int>v;
+//         while(curr!=NULL||st.size()!=0){
+//             if(curr!=NULL){
+//                 st.push(curr);
+//                 curr=curr->left;
+//             }
+//             else{
+//                 TreeNode *temp=st.top()->right;
+//                 if(temp==NULL){
+//                     temp=st.top();
+//                     st.pop();
+//                     v.push_back(temp->val);
+//                     while(st.size()!=0&&st.top()->right==temp){
+//                         temp=st.top();
+//                         v.push_back(temp->val);
+//                         st.pop();
+//                     }
+//                 }
+//                 else{
+//                     curr=temp;
+//                 }
+//             }
+//         }
+//         return v;
+//     }
+// };
+
+
+
+
+
+
+
+
+
