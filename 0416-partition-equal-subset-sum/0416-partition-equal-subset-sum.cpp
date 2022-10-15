299645178531d2411f2016dc1bb4de26c8f486d5
Ayush <87146546+Fabayu@users.noreply.github.com>
2022-10-16 01:55:30 +0530

    Time: 317 ms (64.46%), Space: 11.7 MB (80.11%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     
-    bool is(vector<int>& nums, int sum,int n){
-     bool dp[n+1][sum+1];
-        for(int i = 0 ; i<=n ; i++){
-            for(int j = 0 ; j<=sum ; j++){
-                if(i==0)
-                    dp[i][j] = false;
-                if(j==0)
-                    dp[i][j] = true;
-            }
-        }
-        for(int i = 1 ; i<=n ; i++){
-            for(int j = 1 ; j<=sum ; j++){
-                if(nums[i-1]<=j)
-                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                else
-                    dp[i][j] = dp[i-1][j];
-            }
-        }
-        return dp[n][sum];
+    bool is(vector<int>& arr, int s,int n){
+    
+    
+        bool t[n+1][s+1];
+     for(int i=0;i<n+1;i++){
+         for(int j=0;j<s+1;j++){
+             if(i==0){
+                 t[i][j]=false;
+                 
+             }
+             if(j==0){
+                 t[i][j]=true;
+             }
+         }
+     }
+             for(int i=1;i<n+1;i++){
+         for(int j=1;j<s+1;j++){ 
+             
+              if(arr[i-1]<=j){
+                 t[i][j]=t[i-1][j-arr[i-1]]||t[i-1][j];
+                 
+             }
+             else{
+                 t[i][j]=t[i-1][j];
+               
+             }
+         }
+     }
+     
+     return t[n][s]; 
+    
+     
+     
     }
         
     
