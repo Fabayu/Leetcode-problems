f84fe7c29f5f5104467ee3bc85418911f4dbe51f
Ayush <87146546+Fabayu@users.noreply.github.com>
2022-11-05 09:53:25 +0530

    Added solution - LeetHub

---
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+    vector<int> vis(V,0);
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    pq.push({0,0});
+         int sum=0;
+    while(!pq.empty()){
+   
+        auto it=pq.top();
+        pq.pop();
+        int node=it.second;
+        int pushedw=it.first;
+        if(vis[node]==1)continue;
+        else{
+            vis[node]=1;
+            sum+=pushedw;
+        }
+        for(auto it:adj[node]){
+            int adjnode=it[0];
+            int edgew=it[1];
+            if(!vis[adjnode]){
+                pq.push({edgew,adjnode});
+            }
+        }
+    }
+    return sum;
+        
+        
+        
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
