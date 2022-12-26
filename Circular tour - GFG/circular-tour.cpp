eb228ea3fc220fbb22f28525c3a0d6dec4b1fdb6
Ayush <87146546+Fabayu@users.noreply.github.com>
2022-12-26 15:25:02 +0530

    Added solution - LeetHub

---
+++ Circular tour - GFG/circular-tour.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};
+
+
+// } Driver Code Ends
+/*
+The structure of petrolPump is 
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};*/
+
+/*You are required to complete this method*/
+class Solution{
+  public:
+  
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+       int sump=0;
+       int sumd=0;
+       for(int i=0;i<n;i++){
+           sump+=p[i].petrol;
+           sumd+=p[i].distance;
+           
+       }
+       if(sump<sumd)return -1;
+       
+        int deficit=0;
+
+       int balance=0;
+
+       int start=0;
+       for(int i=0;i<n;i++){
+
+           balance +=p[i].petrol-p[i].distance;
+
+           if(balance<0){
+
+              
+
+               start=i+1;
+
+               balance=0;
+
+           }
+
+       }
+       return start;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        petrolPump p[n];
+        for(int i=0;i<n;i++)
+            cin>>p[i].petrol>>p[i].distance;
+        Solution obj;
+        cout<<obj.tour(p,n)<<endl;
+    }
+}
+
+// } Driver Code Ends
